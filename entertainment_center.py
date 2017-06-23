2ae2407c95ca83905b7f73f5c893731df2c58481
Adham El Banhawy <the.benhawy@gmail.com>
2017-06-23 01:12:43 -0500

    Added youtube trailer attribute via API call in media class

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,31 +1,28 @@
 import movie
 import fresh_tomatoes
 
-toy_story = movie.Movie("Toy Story", "http://a.dilcdn.com/bl/wp-content/uploads/sites/8/2013/02/toy_story_wallpaper_by_artifypics-d5gss19.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc ", "some tagline", "a date")
+toy_story = movie.Movie("Toy Story", """http://a.dilcdn.com/bl/wp-content/uploads/sites/8/2013/02/
+toy_story_wallpaper_by_artifypics-d5gss19.jpg""")
 
-# print toy_story.storyline
+avatar = movie.Movie("Avatar", """https://resizing.flixster.com/wmwaS9C76fnLpML3UAj0i3l6djw=/206x30
+5/v1.bTsxMTE3Njc5MjtqOzE3NDU0OzEyMDA7ODAwOzEyMDA""")
 
-avatar = movie.Movie("Avatar", "https://resizing.flixster.com/wmwaS9C76fnLpML3UAj0i3l6djw=/206x305/v1.bTsxMTE3Njc5MjtqOzE3NDU0OzEyMDA7ODAwOzEyMDA", "https://www.youtube.com/watch?v=cRdxXPV9GNQ", "some tagline", "a date")
+alien = movie.Movie("Alien", """https://fanart.tv/fanart/movies/348/movieposter/alien-527cde786d68c.
+jpg""")
 
-# print avatar.storyline
+lalaland = movie.Movie("La La Land", "http://www.impawards.com/2016/posters/la_la_land_ver8.jpg")
 
-# avatar.show_trailer()
+mortal_kombat = movie.Movie("Mortal Kombat", "http://cdn.sockshare.net/bUYVUiY.png")
 
-alien = movie.Movie("Alien", "https://fanart.tv/fanart/movies/348/movieposter/alien-527cde786d68c.jpg", "https://www.youtube.com/watch?v=LjLamj-b0I8", "some tagline", "a date")
+rocky = movie.Movie("Rocky", "https://ctcmr.files.wordpress.com/2010/12/image-php.jpeg")
 
-# alien.show_trailer()
+movies = [toy_story, avatar, alien, lalaland, mortal_kombat, rocky ]
 
-lalaland = movie.Movie("La La Land", "http://www.impawards.com/2016/posters/la_la_land_ver8.jpg", "https://www.youtube.com/watch?v=0pdqf4P9MB8", "some tagline", "a date")
-
-mortal_kombat = movie.Movie("Mortal Kombat", "http://cdn.sockshare.net/bUYVUiY.png", "https://www.youtube.com/watch?v=6LxGtUWxv0o", "some tagline", "a date")
-
-rocky = movie.Movie("Rocky", "https://ctcmr.files.wordpress.com/2010/12/image-php.jpeg", "https://www.youtube.com/watch?v=7RYpJAUMo2M", "some tagline", "a date")
-
-# movies = [toy_story, avatar, alien, lalaland, mortal_kombat, rocky ]
-
-avatar.get_storyline()
-avatar.get_release_date()
-print lalaland.movie_id
-# fresh_tomatoes.open_movies_page(movies)
+# avatar.get_storyline()
+# avatar.get_release_date()
+# # print lalaland.movie_id
+# print avatar.trailer_youtube_url
+# lalaland.show_trailer()
+fresh_tomatoes.open_movies_page(movies)
 
 # print media.Movie.__doc__python 
--- media.py
+++ media.py
@@ -5,24 +5,45 @@ import requests
 class  Media():
     """ This class provides a way of storing media related information"""
 
-    VALID_RATINGS = [ "G", "PG",  "PG-13", "R"]
-    def __init__(self, title, poster_image, trailer_youtube):
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    def __init__(self, title, poster_image):
         self.title = title
         self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
-        self.api_url = "https://api.themoviedb.org/3/search/movie?api_key=873c09566ed2be62fbd02102e48c399e&query=" + self.title
+
+        # API call to get movie_id which is used in subsequent API calls
+        self.api_url = """https://api.themoviedb.org/3/search/movie?api_key=873c09566ed2be62fbd02102
+        e48c399e&query=""" + self.title
         self.data = requests.get(self.api_url).json()
         self.movie_id = self.data["results"][0]["id"]
+
+        # Api call to get media's youtube trailer key
+        self.youtube_api_url = "https://api.themoviedb.org/3/movie/" + str(self.movie_id) + "/vid" \
+        "eos?language=en-US&api_key=873c09566ed2be62fbd02102e48c399e"
+        self.data2 = requests.get(self.youtube_api_url).json()
+        try:
+            self.youtube_key = self.data2["results"][0]["key"]
+            # Get the youtube key of type Trailer
+            for i in range(len(self.data2["results"])):
+                if self.data2["results"][i]["type"] == "Trailer":
+                    self.youtube_key = self.data2["results"][i]["key"]
+                    break
+        except IndexError:
+            # If json object doesn't contain trailer or is empty returns a "Coming Soon" video
+            print  self.title + " needs to be uploaded manually"
+            self.youtube_key = "0TjxnrWT8Es"
+        self.trailer_youtube_url = "www.youtube.com/watch?v=" + self.youtube_key
+
+
+    def get_storyline(self):
+        query_api_url = "https://api.themoviedb.org/3/movie/" + str(self.movie_id) + """?language=
+        en-US&api_key=873c09566ed2be62fbd02102e48c399e"""
+        details = requests.get(query_api_url).json()
+        self.storyline = details["overview"]
+        # print self.storyline
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 
-    def get_storyline(self):
-        self.movie_id
-        query_api_url = "https://api.themoviedb.org/3/movie/" + str(self.movie_id) + "?language=en-US&api_key=873c09566ed2be62fbd02102e48c399e"
-        details = requests.get(query_api_url).json()
-        self.storyline = details["overview"]
-        print self.storyline
-        
+
     # def get_poster(self):
  
